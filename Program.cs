3281d6a4c9a06e706011564d781214786ea253f4
Ben Spencer <ben.richard.spencer@me.com>
2017-01-16 21:15:00 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Intro;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = getUrls("https://gist.githubusercontent.com/anonymous/d2ec2461468d4a0372db/raw/b1eb88fa20b147deaafa9e38768174d79f705805/gistfile1.txt");
+
+            deleteBlogs();
+
+            foreach (var x in result.Result.Split('\n').Select(x => x).Distinct())
+            {
+                addBlog(new Blog { Url = x });
+            }
+            getBlogs();
+        }
+
+        public static void getBlogs()
+        {
+            using (var db = new BloggingContext())
+            {
+                foreach (var x in db.Blogs)
+                {
+                    Console.WriteLine("Blog Id " + "\t" + x.BlogId + "\t" + "Blog Url " + "\t" + x.Url);
+                }
+            }
+        }
+
+        public static void addBlog(Blog blog)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+        }
+        public static void deleteBlogs()
+        {
+            using (var db = new BloggingContext())
+            {
+                var itemsToDelete = db.Set<Blog>();
+
+                db.Blogs.RemoveRange(itemsToDelete);
+                db.SaveChanges();
+            }
+        }
+
+        public static async Task<String> getUrls(string url)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+
+    }
+}
